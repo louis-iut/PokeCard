c3cd79a4f24a6ac628fcdd5f0d8c7fdcbee7ddcf
Quentin G <quentin01960@orange.fr>
2017-12-11 17:20:51 +0100

    get pokemons for user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,7 @@
 
 namespace App\Controller;
 use Silex\Application;
+use App\Entity\Pokemon;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,6 +28,34 @@ class UserController
         die();
         $user = $app['repository.user']->getByEmail($parameters['email']);
         die();
+    }
+
+
+    public function addPokemonAction(Request $request, Application $app) {
+
+        $parameters = $request->attributes->all();
+        $association = $app['repository.user']->insert($parameters['userID'], $parameters['pokemonID']);
+        
+        $statutCode = 200;
+        $content = json_encode(array('message' => 'pokemon added'));
+        return new Response($content, $statutCode ,['Content-type' => 'application/json']);
+    }
+
+    public function getPokemons(Request $request, Application $app) {
+
+        $parameters = $request->attributes->all();
+        $pokemons = $app['repository.user']->getPokemons($parameters['userID']);
+
+        $pokemonsArray = array();
+        foreach ($pokemons as $pokemon) {
+            $newPokemon = $app['repository.pokemon']->getById($pokemon['pokemonID'], "en");
+            array_push($pokemonsArray, $newPokemon->toArray());
+
+        }
+
+        $statutCode = 200;
+        $content = json_encode($pokemonsArray);
+        return new Response($content, $statutCode ,['Content-type' => 'application/json']);
     }
 
 }
